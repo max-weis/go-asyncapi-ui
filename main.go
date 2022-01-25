c35bafbb18056a26d96088bebc24416784a182eb
Weis, Max <Max.Weis@adesso.de>
2022-01-25 10:11:31 +0100

    add asyncapi ui path

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"html/template"
+	"io"
+	"net/http"
+	"os"
+)
+
+// TemplateRenderer is a custom html/template renderer for Echo framework
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+// Render renders a template document
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func main() {
+	e := echo.New()
+
+	renderer := &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("*.html")),
+	}
+	e.Renderer = renderer
+
+	asyncapi_url := os.Getenv("ASYNCAPI_URL")
+	if asyncapi_url == "" {
+		asyncapi_url = "http://localhost:8000/static/asyncapi.yml"
+	}
+
+	e.Static("/static", ".")
+
+	// Named route "foobar"
+	e.GET("/asyncapi-ui", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index.html", map[string]interface{}{
+			"url": asyncapi_url,
+		})
+	})
+
+	e.Logger.Fatal(e.Start(":8000"))
+}
